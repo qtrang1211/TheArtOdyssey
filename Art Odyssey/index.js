5e1bb62b01eb825ed9807818bf6088eb6cc0da89
TejasviParmar <152898258+TejasviParmar@users.noreply.github.com>
2024-04-07 11:23:33 +0100

    (nav bar, scroll button works now!!!)

--- Art Odyssey/index.js
+++ Art Odyssey/index.js
@@ -108,8 +108,6 @@ document.addEventListener("DOMContentLoaded", function () {
       console.log("Accepted Cookie Usage");
     });
 
-    //-----------------BACK TO TOP-----------
-
     // Function to create a cookie
     function createCookie(c_name, c_value, c_days) {
       // Your createCookie function implementation
@@ -120,32 +118,23 @@ document.addEventListener("DOMContentLoaded", function () {
       // Your readCookie function implementation
     }
   });
-
-  /*
-  ======================================
-            BACK TO TOP LINK
-======================================
-*/
-
-  // When the user scrolls down 20px from the top of the document, show the button
-  window.onscroll = function () {
-    scrollFunction();
-  };
-
-  function scrollFunction() {
-    if (
-      document.body.scrollTop > 20 ||
-      document.documentElement.scrollTop > 20
-    ) {
-      document.getElementById("myBtn").style.display = "block";
-    } else {
-      document.getElementById("myBtn").style.display = "none";
-    }
-  }
-
-  // When the user clicks on the button, scroll to the top of the document
-  function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-  }
 });
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
